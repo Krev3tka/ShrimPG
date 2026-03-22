b6a282dcb7cdf939445f03eb571f205d53783280
Krev3tka <laskofilipp@gmail.com>
2026-03-22 16:49:37 +0300

    chore: add initial database migration

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,14 +1,17 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(64) NOT NULL
+    name VARCHAR(64) NOT NULL,
+    master_hash BYTEA,
+    master_salt BYTEA
 );
 
 CREATE TABLE IF NOT EXISTS passwords (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id),
     salt BYTEA NOT NULL,
-    service VARCHAR(64) NOT NULL UNIQUE,
+    service VARCHAR(64) NOT NULL,
     encrypted_data BYTEA NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    master_check TEXT DEFAULT 'OK'
+    master_check TEXT DEFAULT 'OK',
+    UNIQUE(user_id, service)
 );
